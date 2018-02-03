01ea4af0be85d435538b19cb85ed60b5ed339f20
ogunlao <ogunlao@yahoo.com>
2018-02-03 21:12:35 +0100

    Edited settings to use postgresql

--- talkatif/settings.py
+++ talkatif/settings.py
@@ -143,10 +143,15 @@ WSGI_APPLICATION = 'talkatif.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
