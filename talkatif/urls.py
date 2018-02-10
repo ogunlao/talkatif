b1c978cde415b7b15f68b6e52a15228e3b2d67b9
ogunlao <ogunlao@yahoo.com>
2018-02-10 11:16:28 +0100

    Fixed 500 error with django compressor

--- talkatif/urls.py
+++ talkatif/urls.py
@@ -67,7 +67,7 @@ handler500 = 'discourse.views.handler500'
 from django.conf.urls.static import static
 from django.conf import settings
 
-#if settings.DEBUG == True:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
