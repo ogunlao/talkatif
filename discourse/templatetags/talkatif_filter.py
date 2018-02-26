c3a092b5abaee1987bf1bd93b1bd9b126d90fb51
ogunlao <ogunlao@yahoo.com>
2018-02-26 14:34:37 +0100

    My Django Girls app, first commit

--- discourse/templatetags/talkatif_filter.py
+++ discourse/templatetags/talkatif_filter.py
@@ -24,11 +24,12 @@ import json
 API_KEY = settings.GOO_API_KEY
 @register.filter()
 def goo_shorten_url(url):
-    post_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(API_KEY)
-    payload = {'longUrl': url}
-    headers = {'content-type': 'application/json'}
-    r = requests.post(post_url, data=json.dumps(payload), headers=headers)
-    try:
-        return r.json()["id"]
-    except:
-        return url
+    return url
+    # post_url = 'https://www.googleapis.com/urlshortener/v1/url?key={}'.format(API_KEY)
+    # payload = {'longUrl': url}
+    # headers = {'content-type': 'application/json'}
+    # r = requests.post(post_url, data=json.dumps(payload), headers=headers)
+    # try:
+    #     return r.json()["id"]
+    # except:
+    #     return url
